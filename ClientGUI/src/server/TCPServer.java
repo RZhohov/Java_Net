0f6fcb47ed0f9bd4205cafd22d9ade6bb677429a
Roman Zhohov <romanzhohov@gmail.com>
2017-09-16 05:10:36 -0700

    Logging functionality added

--- ClientGUI/src/server/TCPServer.java
+++ ClientGUI/src/server/TCPServer.java
@@ -1,6 +1,7 @@
 package server;
 
 import java.io.BufferedReader;
+import java.util.logging.*;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -11,6 +12,21 @@ import java.net.Socket;
 
 public class TCPServer implements Runnable {
 	
+   private static Logger logger = Logger.getLogger(TCPServer.class.getName());
+   private static FileHandler fh = null;
+   private static Level log_level = Level.ALL; // OFF-SEVERE-WARNING-INFO-CONFIG-FINE-FINER-FINEST-ALL
+   public static void init(){
+	   try {
+	   fh=new FileHandler("loggerExample.log", false);
+	   } catch (SecurityException | IOException e) {
+	   e.printStackTrace();
+	   }
+	   Logger l = Logger.getLogger("");
+	   fh.setFormatter(new SimpleFormatter());
+	   l.addHandler(fh);
+	   }
+   
+   
    private static int PORT = 8090;
    Socket csocket;
    TCPServer(Socket csocket) {
@@ -20,6 +36,7 @@ public class TCPServer implements Runnable {
    public void run() {
       try {
     	//I/O streams  
+    	logger.info("Creating I/O Streams");
   		InputStream in = csocket.getInputStream(); 
   		OutputStream out = csocket.getOutputStream();
   		BufferedReader din = new BufferedReader(new InputStreamReader(in));
@@ -29,25 +46,48 @@ public class TCPServer implements Runnable {
   		{
   		String command = din.readLine();
   			if (command.equals("quit"))
+  			{
+  				logger.info("User quits");
   				break;
-  		UnixExecuter cli = new UnixExecuter();
-  		String response = cli.execute(command);
-  		pstream.println(response);
+  			}
+  			else {
+  				logger.fine("User executes command " + command);
+  				UnixExecuter cli = new UnixExecuter();
+  		  		String response = cli.execute(command);
+  		  		pstream.println(response);
+  			}
   		}
+  		 logger.info("Stream closed, Socket closed");
          pstream.close();
          csocket.close();
       } catch (IOException e) {
-         System.out.println(e);
+    	 logger.severe(e.getMessage());
       }
    }
    
    
-   public static void main(String args[]) throws Exception { 
-	      ServerSocket ssock = new ServerSocket(PORT);
+   public static void main(String args[]) {
+	   	  init();
+	   	  logger.setLevel(log_level);
+	   	  
+	   	  logger.info("Creating Server Socket");
+	      ServerSocket ssock = null;
+		try {
+			ssock = new ServerSocket(PORT);
+		} catch (IOException e) {
+			logger.severe(e.getMessage());
+		}
 	      System.out.println("Listening");
 	      
 	      while (true) {
-	         Socket sock = ssock.accept();
+	         Socket sock = null;
+			try {
+				sock = ssock.accept();
+				logger.info("Accepting connection from client");
+			} catch (IOException e) {
+				logger.severe(e.getMessage());
+			}
+			logger.info("Creating new thread for client");
 	         System.out.println("Connected");
 	         new Thread(new TCPServer(sock)).start();
 	      }
